f5f3d2d8566ca03d109d70996ad5a3411fa08e94
Alasdair Cooper <alasdair.code@hotmail.com>
2025-09-13 16:56:22 +0100

    expose endpoints to public

--- src/app-host/Program.cs
+++ src/app-host/Program.cs
@@ -2,6 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var gitHubApiTokenParameter = builder.AddParameter("github-api-token", true);
 
-builder.AddProject<Projects.AlasdairCooper_CSharpLangLdmFeed_Api>("api").WithEnvironment("GITHUB_API_TOKEN", gitHubApiTokenParameter);
+builder.AddProject<Projects.AlasdairCooper_CSharpLangLdmFeed_Api>("api")
+    .WithEnvironment("GITHUB_API_TOKEN", gitHubApiTokenParameter)
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
