b3aaf6c443a22ca2663e9bd7d21088e757b490a3
Alasdair Cooper <alasdair.code@hotmail.com>
2025-09-24 22:01:52 +0100

    make aspire network external

--- src/app-host/Program.cs
+++ src/app-host/Program.cs
@@ -1,6 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddDockerComposeEnvironment("docker");
+builder.AddDockerComposeEnvironment("docker")
+    .ConfigureComposeFile(
+        static x =>
+        {
+            foreach (var (_, value) in x.Networks.Where(static x => x.Key == "aspire"))
+            {
+                value.External = true;
+            }
+        });
 
 var gitHubApiTokenParameter = builder.AddParameter("github-api-token", true);
 
